99c6a77c08854b0e00eba10ec7edff8f93d1efe4
jong lee <lsljw94@daum.net>
2022-08-08 15:40:48 +0900

    플러스 마이너스 버튼 jquery

--- js/response1.js
+++ js/response1.js
@@ -18,7 +18,28 @@ $(document).ready(function(){
     });
     
     $(".thum-wrap .thum:gt(3)").hide();
+    // 클래스가 thum인 요소의 개수가 3보다 큰 것들은 숨김
+    var bool=true;
     $(".more").click(function(){
+
         $(".thum-wrap .thum:gt(3)").fadeToggle();
+        // 요소의 개수가 3보다 큰 것들은 보임
+        // $(this).text("-");
+        // $(this).html("<i class='fa fa-minus'></i>")
+        // $(this).html('<i class="fa fa-minus"></i>')
+        // $(".more>i").attr("class","fa fa-minus")
+        // $(this).children("i").attr("class","fa fa-minus");
+        // $(".more i").attr("class","fa fa-minus")
+        // $(this).find("i").attr("class","fa fa-minus");
+        if(bool){
+            $(this).children("i").removeClass("fa-plus")
+            $(this).children("i").addClass("fa-minus")
+            bool=false;
+        }else{
+            $(this).children("i").removeClass("fa-minus")
+            $(this).children("i").addClass("fa-plus")
+            bool=true;
+        }
+        
     });
 });
